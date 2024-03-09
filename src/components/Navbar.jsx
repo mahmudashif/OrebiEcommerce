a2331f9c6aabce47b82e8eb93fef74c806f0263b
ashifmahmud <mdamahmud2000@gmail.com>
2024-03-10 02:59:35 +0600

    navbar done without responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,17 +4,17 @@ import logo from '../assets/Logo.png'
 const Navbar = () => {
   return (
     <div>
-      <nav className="font-dm max-w-container mx-auto bg-red-300 py-8">
+      <nav className="font-dm max-w-container mx-auto py-8">
         <div className="flex">
           <div className="w-2/5">
             <img src={logo} alt="" />
           </div>
-          <ul className="flex w-3/5 gap-12 capitalize justify-center">
-            <li>home</li>
-            <li>shop</li>
-            <li>about</li>
-            <li>contacts</li>
-            <li>journel</li>
+          <ul className="text-[#767676] text-lg flex w-3/5 gap-12 capitalize justify-center">
+            <li className=' hover:text-[#262626]'>home</li>
+            <li className=' hover:text-[#262626]'>shop</li>
+            <li className=' hover:text-[#262626]'>about</li>
+            <li className=' hover:text-[#262626]'>contacts</li>
+            <li className=' hover:text-[#262626]'>journel</li>
           </ul>
         </div>
       </nav>
